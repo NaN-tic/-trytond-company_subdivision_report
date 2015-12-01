03423b9710a5d71919ba4dd85102e61f94f56c7b
resteve <resteve@zikzakmedia.com>
2015-12-01 11:06:04 +0100

    Tests

--- tests/test_company_subdivision_report.py
+++ tests/test_company_subdivision_report.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python
-# This file is part company_subdivision_report module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the company_subdivision_report module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CompanySubdivisionReportTestCase(unittest.TestCase):
+class CompanySubdivisionReportTestCase(ModuleTestCase):
     'Test Company Subdivision Report module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('company_subdivision_report')
-
-    def test0005views(self):
-        'Test views'
-        test_view('company_subdivision_report')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'company_subdivision_report'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            CompanySubdivisionReportTestCase))
+        CompanySubdivisionReportTestCase))
     return suite
